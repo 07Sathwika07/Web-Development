ca1b57ed48b544f48330ab4d7ee2b334aa7d20ff
07Sathwika07 <sathwikakalvakuntla07@gmail.com>
2026-08-12 22:23:48 +0530

    Add files via upload

---
+++ Register.js
@@ -0,0 +1,25 @@
+import "../App.css";
+
+function Register(){
+
+return(
+
+<div className="card">
+
+<h1>Register</h1>
+
+<input type="text" placeholder="Full Name"/>
+
+<input type="email" placeholder="Email"/>
+
+<input type="password" placeholder="Password"/>
+
+<button>Register</button>
+
+</div>
+
+)
+
+}
+
+export default Register;
